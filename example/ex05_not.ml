f99585fab8d179df173cc2f6052a6b27684065fa
Xavier Clerc <xclerc@janestreet.com>
2017-09-15 14:51:29 +0100

    Augmented example for not patterns.

--- example/ex05_not.ml
+++ example/ex05_not.ml
@@ -11,6 +11,22 @@ let print_desc : string -> Parsetree.expression -> unit = fun value expr ->
   | _ ->
     Printf.printf "%S is ???\n" value
 
+let print_desc' : string -> Parsetree.expression -> unit = fun value expr ->
+  let open Viewast.Parseview in
+  match%view expr with
+  | Pexp_constant (Not (Pconst_integer _ | Pconst_float _)) ->
+    Printf.printf "%S is not a number\n" value
+  | Pexp_constant (Pconst_integer _) ->
+    Printf.printf "%S is an integer\n" value
+  | Pexp_constant (Pconst_float _) ->
+    Printf.printf "%S is a float\n" value
+  | _ ->
+    Printf.printf "%S is ???\n" value
+
+let print_descs value expr =
+  print_desc  value expr;
+  print_desc' value expr
+
 
 let run () =
   let one =
@@ -33,9 +49,9 @@ let run () =
     Ast_helper.Exp.constant
       (Ast_helper.Const.float "3.14")
   in
-  print_desc "1"    one;
-  print_desc "3"    three_int;
-  print_desc "3l"   three_int32;
-  print_desc "3.14" pi;
-  print_desc "abc"  abc;
+  print_descs "1"    one;
+  print_descs "3"    three_int;
+  print_descs "3l"   three_int32;
+  print_descs "3.14" pi;
+  print_descs "abc"  abc;
   ()
